0848318a77cde2b0ab0865e81db39b52850d7e36
Lynn Kim <lynn.kim@brandung.de>
2023-07-03 00:10:26 +0200

    disallow reusing the score line

--- src/components/Game.js
+++ src/components/Game.js
@@ -60,7 +60,10 @@ class Game extends Component {
   doScore(rulename, ruleFn) {
     // evaluate this ruleFn with the dice and score this rulename
     this.setState((st) => ({
-      scores: { ...st.scores, [rulename]: ruleFn(this.state.dice) },
+      scores: {
+        ...st.scores,
+        [rulename]: ruleFn(this.state.dice),
+      },
       rollsLeft: NUM_ROLLS,
       locked: Array(NUM_DICE).fill(false),
     }));
